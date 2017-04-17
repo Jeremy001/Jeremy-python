e77e055638dc194647d6059940cc4e56d1aca276
jeremy001 <diyiduan@126.com>
2017-04-18 07:26:59 +0800

    update some file

--- py04_basic/basic06_dict and set.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-# dict
-# python内置了字典；dict的支持，全称dictionary，以键值对存储，具有极快的查询速度
-# 如果用list实现，则需要两个list：
-names = ['Micheal', 'Jeremy', 'Sarah']
-scores = [90, 84, 65]
-如果给定一个名字，要查找对应的成绩，则需要先从names中找到对应的位置，然后到
