c819957cab6d9434e23ff7daf9b09b1e3b37e04c
jeremy001 <diyiduan@126.com>
2017-04-20 15:00:46 +0800

    update python file

--- python course liaoxuefeng/py05_function/function05_recursive function.py
+++ python course liaoxuefeng/py05_function/function05_recursive function.py
@@ -28,7 +28,12 @@ def fact_iter(n, product):
 
 # practice
 
-
+def move(n, a, b, c):
+    if n == 1:
+        print('move', a, '--->', c)
+    move(n -1, a, c, b)
+    print('move', a, '--->', c)
+    move(n -1, b, a, c)
 
 
 
---
+++ python course liaoxuefeng/py06_advanced features/advanced features01_slice.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 取一个list或tuple中的部分元素
+# 笨办法：
+L1 = ('Jeremy', 'Sarah', 'Micheal', 'Steve', 'Adam')
+L2 = ['Jeremy', 'Sarah', 'Micheal', 'Steve', 'Adam']
+print(L1[0], L1[2], L1[4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
