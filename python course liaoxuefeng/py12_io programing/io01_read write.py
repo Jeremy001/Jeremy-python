3fcb188774188b644211aedd4a94d0fab10ec268
jeremy001 <diyiduan@126.com>
2017-04-30 22:02:27 +0800

    update some python files

--- python course liaoxuefeng/py12_io programing/io01_read write.py
+++ python course liaoxuefeng/py12_io programing/io01_read write.py
@@ -25,38 +25,31 @@ finally:
 with open('text.txt', 'r') as f:
     print(f.read())
 
+# read()：调用文件中的所有内容；
+# read(size):调用设定大小的文本内容：size个字节
+# readlines():每次读取一行；
+# 如果文件很小，用read()一次性读取所有内容
+# 如果文件大写未知，用read(size)，设置size，保险一些
+# 如果希望按行读取，用readlines()
+with open('text.txt', 'r') as f:
+    for line in f.readlines():
+        print(line.strip())
 
+# 读取二进制文件，如图片或视频，'r'改成'rb'
 
+# 字符编码
+# 读取非utf-8编码的文件，需要给open()加上encoding参数
+f = open('text.txt', 'r', encoding = 'gbk')
+f.read()
+# 忽略错误
+f = open('text.txt', 'r', encoding = 'gbk', errors = 'ignore')
+f.read()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 写文件，格式一样， 区别是'r'改成'w'(一般文件)/'wb'(二进制文件)
+f = open('text.txt', 'w')
+f.write('oh my god!')
+f.close()
+# 写文件时，操作系统往往不会直接写入磁盘，而是先写入内存中缓存起来，然后有空的时候慢慢写入磁盘；
+# 只有调用了close()方法时，操作系统才能保证把数据全部写入了磁盘
+with open('text.txt', 'w') as f:
+    f.write('haha, I\'m coming!')
