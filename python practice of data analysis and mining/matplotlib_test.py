db731a43b2712734c53f7d437396d83ba4179b7e
jeremy001 <diyiduan@126.com>
2017-07-05 22:30:18 +0800

    learn python every day

--- python practice of data analysis and mining/matplotlib_test.py
+++ python practice of data analysis and mining/matplotlib_test.py
@@ -7,8 +7,15 @@ from matplotlib import pyplot as plt
 x = np.linspace(-np.pi, np.pi, 256, endpoint = True)
 c,s = np.cos(x),np.sin(x)
 plt.figure(1)
-plt.plot(x, c)
-plt.plot(x, s)
+plt.plot(x, c, color = "blue", linewidth = 1.0, linestyle = "-", label = "COS", alpha = 0.5)
+plt.plot(x, s, "r*", label = "SIN")
+plt.title("COS & SIN")
+ax = plt.gca()
+ax.splines["right"].set_color("none")
+ax.splines["top"].set_color("none")
+ax.splines["left"].set_position(("data".0))
+ax.splines["bottom"].set_position(("data".0))
+
 plt.show()
 
 
