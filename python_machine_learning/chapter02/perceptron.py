838a8e6e9916a69dbfc676c3efba8318b49ca53a
jeremy001 <diyiduan@126.com>
2017-08-03 13:29:24 +0800

    learn python hard everyday

--- python_machine_learning/chapter02/perceptron.py
+++ python_machine_learning/chapter02/perceptron.py
@@ -88,12 +88,46 @@ X = df.iloc[0:100, [0, 2]].values
 ppn = Perceptron(eta = 0.1, n_iter = 10)
 ppn.fit(X, y)
 
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
-plt.xlabel('Epoches')
-plt.ylabel('Numebr of misclassifications')
-plt.show()
+# plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker = 'o')
+# plt.xlabel('Epoches')
+# plt.ylabel('Numebr of misclassifications')
+# plt.show()
 # 从结果中看到，当第六次迭代时，算法就收敛了，且对训练集的分类准确率是100%
 
 from matplotlib.colors import ListedColormap
+
+def plot_decision_region(X, y, classifier, resolution = 0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
+                                                np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], 
+                            alpha = 0.8, c = cmap(idx), 
+                            marker = markers[idx], label = cl)
+
+    plot_decision_region(X, y, classifier = ppn)
+    plt.xlabel('sepal length(cm)')
+    plt.ylabel('petal.length(cm)')
+    plt.legend(loc = 'upper left')
+    plt.show()
+
+
+
+
 
 
--- python_machine_learning/chapter03_Scikit-learn/logistic_regression.py
+++ python_machine_learning/chapter03_Scikit-learn/logistic_regression.py
@@ -14,8 +14,31 @@ plt.plot(phi_z)
 plt.show()
 
 # load dataset ======================================
-iris = 
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+np.unique(y)
+
 # data pre-processing =================================
+# 1.split data into train and test datasets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+# 2.standardization 
+from sklearn.preprocessing import StardardScaler
+sc = StardardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# modeling ========================================
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression(C = 1000.0, random_state = 0)
+lr.fit(X_train_std, y_train)
+
+plot_decission_regions(X_combined_std, y_combined, classifier = lr, test_idx = range(105, 150))
+plt.show()
+
 
 
 
