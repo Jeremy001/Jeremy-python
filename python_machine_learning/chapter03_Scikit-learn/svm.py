16f18066fe8750440397667f4301f8f80b1a25e9
jeremy001 <diyiduan@126.com>
2017-08-05 19:48:15 +0800

    learn python hard everyday

---
+++ python_machine_learning/chapter03_Scikit-learn/svm.py
@@ -0,0 +1,95 @@
+# encoding = utf-8
+
+# load packages ========================================
+import numpy as np
+from sklearn import datasets
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# load datasets =========================================
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+
+# data pre-processing ====================================
+# split data into train and test datasets
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+# standardization
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# modeling ============================================
+# 线性可分，用线性SVM
+from sklearn.svm import SVC
+svm = SVC(kernel = 'linear', C = 1.0, random_state = 0)
+svm.fit(X_train_std, y_train)
+
+
+# define plot_decision_region function ========================
+from matplotlib.colors import ListedColormap
+def plot_decision_region(X, y, classifier, resolution = 0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                                                np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1],
+                            alpha = 0.8, c = cmap(idx),
+                            marker = markers[idx], label = cl)
+
+plot_decision_region(X_train_std, y_train, classifier = svm)
+plt.show()
+
+
+# 随机梯度版的逻辑回归，感知机和支持向量机
+from sklearn,linear_model import SGDClassifier
+ppn = SGDClassifier(loss = 'perceptron')
+lr = SGDClassifier(loss = 'log')
+svm = SGDClassifier(loss = 'hinge')
+
+
+
+# 核SVM解决非线性
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
