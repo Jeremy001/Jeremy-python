1f14bc37724514e134d4e94147b39db8ff282c85
Jeremy001 <diyiduan@126.com>
2018-08-31 14:30:41 +0800

    trendiano

--- 01_data_processing/web_scraping/trendiano_goods.R
+++
@@ -1,7 +0,0 @@
-trendiano_goods_info = read.csv(file = '/Users/yunshan/Documents/Jeremy-python/01_data_processing/web_scraping/trendiano_goods_info.csv', 
-                                sep = '\t', 
-                                encoding = 'utf-8')
-
-View(trendiano_goods_info)
-
-
